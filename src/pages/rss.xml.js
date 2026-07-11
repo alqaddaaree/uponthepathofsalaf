e226944aebc667e4a6559cd2d8b25c136ef0f867
Abderrahmene Mihoub <abderrahmene_mhb@proton.me>
2026-07-11 21:41:26 +0100

    fixed rss page data body

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -5,15 +5,20 @@ export async function GET() {
   const sortedPosts = posts.sort((a, b) => new Date(b.data.date) - new Date(a.data.date));
   const siteUrl = import.meta.env.SITE || 'https://uponthepathofsalaf.vercel.app';
 
-  const items = sortedPosts.map(post => `
+  const items = sortedPosts.map(post => {
+    // Use post.data.body instead of post.body
+    const body = post.data.body || '';
+    const description = body.slice(0, 500).replace(/\n/g, ' ').trim();
+    
+    return `
     <item>
       <title><![CDATA[${post.data.title}]]></title>
-      <link>${siteUrl}/posts/${post.data.id}</link>
-      <guid>${siteUrl}/posts/${post.data.id}</guid>
+      <link>${siteUrl}/posts/${post.id}</link>
+      <guid>${siteUrl}/posts/${post.id}</guid>
       <pubDate>${new Date(post.data.date).toUTCString()}</pubDate>
-      <description><![CDATA[${post.body.slice(0, 500).replace(/\n/g, ' ').trim()}]]></description>
+      <description><![CDATA[${description}]]></description>
     </item>
-  `).join('');
+  `}).join('');
 
   const rss = `<?xml version="1.0" encoding="UTF-8" ?>
   <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
